337167336f665ea2917a7f69cffde4006f7c4343
Javi Martínez-López <112853494+Javi-Martinez-Lopez@users.noreply.github.com>
2024-05-30 23:49:49 +0200

    Add files via upload

---
+++ R/GhibliBrewer(1).R
@@ -0,0 +1,429 @@
+Ghibli <- list('Totoro-Umbrella'=list(c('#6B97AA','#40789A','#20537C','#2E3E54','#192942'), c(2,4,1,5,3), colorblind=T),
+               'Howl-Horizon'=list(c('#E2A04C','#A24854','#4672A3','#E7565B', '#699D53', '#F0C8A4'), c(3,5,2,1,4,6), colorblind=F),
+               'Mononoke-Hug'=list(c('#BAB1AA', '#D0AD97', '#C38D8B', '#B4404D', '#81271E', '#582921', '#382E37'), c(4,7,3,6,1,5,2), colorblind=T),
+               'Mononoke-Knife'=list(c('#E1C7B8', '#CE7301', '#B83D2B', '#A40001', '#810A10', '#73121B'), c(5,6,3,1,4,2), colorblind=T),
+               'Heron-Parrots'=list(c('#48C6DD', '#214D36', '#E7BE6C', '#D17C77', '#ACABA7', '#326060', '#8EB25A', '#45382F'), c(2,6,1,3,7,8,4,5), colorblind=F),
+               'Heron-Hug'=list(c('#A3D9D8', '#C25830', '#F0E991', '#404A49', '#A7414F', '#43522B'), c(4,1,2,3,5,6), colorblind=F),
+               'Porco-Plane'=list(c('#012F4F', '#C79F5A', '#458157', '#422015', '#2D7195', '#B92E03'), c(2,4,5,3,6,1), colorblind=F),
+               'Porco-Cave'=list(c('#7F604F', '#8A7966', '#E1D2B4', '#E7DFD2', '#869898', '#212D2C', '#0B0B08'), c(3,5,1,7,6,2,4), colorblind=T),
+               'Chihiro-Dragon'=list(c('#467271', '#547763', '#7AA381', '#BAC4BC', '#9AB1C4', '#027297', '#2E5069', '#2C4059'), c(4,6,2,8,7,1,5,3), colorblind=F),
+               'Chihiro-Lump'=list(c('#252628', '#EBE9DA', '#80D991', '#F18992', '#EADA67'), c(4,5,1,3,2), colorblind=T),
+               'Chihiro-Bridge'=list(c('#7EB3D4', '#C09C78', '#6DC199', '#ECDA86', '#423E3F', '#DD575E'), c(4,1,3,5,6,2), colorblind=F),
+               'Howl-Terrace'=list(c('#9CC9E8', '#4AA1D6', '#3B6A96',  '#EDADAE', '#ED70BE', '#D14047', '#C6A2C9', '#8D5789', '#603956'), c(7,1,4,8,5,2,6,3,9), colorblind=F),
+               'Chihiro-NoFace'=list(c('#D4AD74', '#2A282B', '#849862', '#ED8779', '#998BA2'), c(2,1,4,5,3), colorblind=F),
+               'Sky-Grass'=list(c('#A0624C', '#3A6E89', '#E35D61', '#288062', '#E9AE6A'), c(5,1,3,4,2), colorblind=F),
+               'Sky-Necklace'=list(c('#8FBFB7', '#7099B1','#3A6494','#212F49'), c(2,4,1,3), colorblind=T),
+               'Howl-FireDemon'=list(c('#87452F', '#CB6536', '#F7AF3C', '#EE912A', '#F26752', '#D23931'), c(6,3,4,1,5,2), colorblind=F),
+               'Howl-Peace'=list(c('#6B375A', '#F7C6C4', '#8BC2CB', '#ABC770', '#E88053', '#409CBE', '#ECCB6E'), c(2,4,6,3,1,5,7), colorblind=F),
+               'Nausicaa-Pilot'=list(c('#413642', '#5D93A7', '#9F4946', '#2A6026', '#BA875E'), c(5,4,1,3,2), colorblind=F),
+               'Nausicaa-Bug'=list(c('#E6E8DC', '#4A6259', '#6BA4AD', '#D7C07D', '#6B1231'), c(5,2,4,3,1), colorblind=T),
+               'Howl-Star'=list(c('#FFFDD2', '#FCEE99', '#EBAC61', '#A1633B', '#265366', '#063C5A', '#042B48'), c(7,3,1,5,6,2,4), colorblind=T),
+               'Totoro-Mouth'=list(c('#3A4C51', '#9A505B', '#DDAF65', '#6EA276', '#D16D87'), c(3,5,2,4,1), colorblind=F),
+               'Ponyo-Joy'=list(c('#F0BEC9', '#B583C2', '#609834', '#6BBBE0', '#8BC35E', '#DB785B', '#53B6E1'), c(7,4,2,5,6,3,1), colorblind=F),
+               'Totoro-House'=list(c('#BD5067', '#0F87A9', '#6C9551', '#D9BE65', '#C87531'), c(2,4,3,5,1), colorblind=F),
+               'Ponyo-Ramen'=list(c('#E1868B', '#E8CB87', '#9AC091', '#8AC0D2', '#D9B494'), c(1,2,3,4,5), colorblind=F),
+               'Ponyo-Boat'=list(c('#B03641', '#236096', '#E7C257', '#58AC5F', '#FF8E30', '#E4696C'), c(2,1,3,5,4,6), colorblind=F),
+               'Totoro-Catbus'=list(c('#6C5040', '#B99366', '#D4BD6A', '#D3CAA1', '#898432', '#565C3A', '#2F3B37'), c(4,1,6,3,7,2,5), colorblind=T),
+               'Mononoke-Guardian'=list(c('#691323', '#B75759', '#CD9A96', '#DED7C5', '#497166', '#2A5045', '#232935'), c(4,1,6,3,7,2,5), colorblind=F),
+               'Pompoko-Fortune'=list(c('#881023', '#7A584C', '#144475', '#ECBF00', '#E9D9C9', '#1E2827', '#707808', '#C57422', '#4377A9'), c(2,9,3,1,7,8,4,5,6), colorblind=F),
+               'Totoro-School'=list(c('#7C8EA4', '#415383', '#A1A862', '#AE4355', '#CF7C06', '#77668A', '#D8B756', '#396A6E', '#D1999A', '#8DBDA7'), c(2,9,5,6,3,4,8,10,1,7), colorblind=F),
+               'Wind-Timer'=list(c('#393D40', '#2C4C61', '#315973', '#4B91B5', '#72C3D6', '#CFC0D7', '#E4D2D2'), c(6,3,7,1,4,2,5), colorblind=T),
+               'Yamadas'=list(c('#D9CCE0', '#A0A7C3', '#FFB4C1', '#B3DABB', '#FFFF81'), c(2,5,4,3,1), colorblind=F),
+               'Earthsea-Silk'=list(c('#342E2E', '#785A8E', '#588F4C', '#C59854', '#A12F35'), c(4,1,5,2,3), colorblind=T),
+               'Heron-Jelly'=list(c('#6F3953', '#B21D3D', '#EB3D56', '#F47C7B', '#9BBE88', '#61E0EF'), c(2,1,4,3,5,6), colorblind=F),
+               'Arrietty-Garden'=list(c('#CADEC7', '#A2D461', '#76AC34', '#4F8526', '#315514'), c(2,4,3,5,1), colorblind=T),
+               'Earthsea-Lightning'=list(c('#FEC7FE', '#A952DA', '#7246A7', '#3E3143'), c(2,1,4,3), colorblind=F),
+               'Marnie-Bedroom'=list(c('#46B1DF','#B5687C','#42785E','#C69B53','#825D38','#51976E','#546488','#DBC063','#AD5C4E'), c(1,3,7,2,4,8,9,6,5), colorblind=F),
+               'Cat-Vals'=list(c('#1A627A', '#F0B865', '#568C3A', '#3A3F38', '#F6E76D', '#98302F'), c(1,6,4,2,3,5), colorblind=F),
+               'Arrietty-Bedroom'=list(c('#4F313B', '#793499', '#776DA1', '#BF959B', '#C97B4B', '#AE7D49', '#608FAD', '#D4D14E'), c(4,1,7,3,8,2,5,6), colorblind=F),
+               'Cat-Prince'=list(c('#CE5327', '#FEE349', '#B661BF', '#4A7DC2', '#828A3E', '#C09470', '#585187', '#417E45', '#27271B'), c(1,2,4,7,9,8,3,5,6), colorblind=F),
+               'Cat-Broom'=list(c('#DB3F7E', '#3BA5CE', '#E1D368', '#EE7F47', '#6CA05A', '#22438A'), c(1,2,4,3,5,6), colorblind=F),
+               'Mark-Car'=list(c('#0F6799', '#DCB938', '#748B81', '#C05800', '#A5B976', '#161F28'), c(2,1,4,3,5,6), colorblind=T),
+               'Heron-Troop'=list(c('#984756', '#EB908D', '#E6CCB3', '#EECD7E', '#E99731', '#C76A31'), c(4,2,6,5,3,1), colorblind=F),
+               'Yesterday-Park'=list(c('#102B1B', '#259B77', '#A0C39C', '#D9573E', '#80AE8A', '#72A25F', '#355B46'), c(6,5,2,1,4,7,3), colorblind=F),
+               'Howl-Breakfast'=list(c('#ABC45F', '#C4824E','#6F5E7F', '#B1B2AA', '#C54D43', '#5577A0', '#7B4249', '#EBC86C'), c(1,6,2,8,3,5,7,4), colorblind=F),
+               'Marnie-Train'=list(c('#027E78', '#CE832E', '#9D3131', '#5C95B2', '#FD5539', '#F7D379', '#004E8C', '#00FC2C'), c(5,6,8,7,2,4,3,1), colorblind=F),
+               'Howl-Castle'=list(c('#4B383B', '#A1464F', '#6E9B5E', '#1B87B8', '#20313A'), c(1,3,4,2,5), colorblind=F),
+               'Kiki-Beach'=list(c('#3273A8', '#D6545B', '#518A5F', '#C50034', '#3B3256'), c(2,5,4,1,3), colorblind=F),
+               'Cat-Bubble'=list(c('#FBCF77', '#FCE090', '#FFF0A3', '#D8ECF5', '#DFB1FE', '#C05CFD', '#9730AF'), c(1,6,4,3,5,7,2), colorblind=T),
+               'Wind-Kiss'=list(c('#C8D563','#EBB8C0','#8EC83C', '#D5D3C7', '#F2C27E',  '#B08D71', '#009CC5', '#1D8F4C'), c(8,4,3,6,1,7,2,5), colorblind=F),
+               'Marnie-Boat'=list(c('#E7B36A', '#ECC36F', '#F3D980', '#F3ECC0', '#ECC6B8', '#F8B6A8', '#AC748E', '#886F8F'), c(1,5,7,3,4,6,8,2), colorblind=F),
+               'Chihiro-Magic'=list(c('#517D42', '#477CB0',  '#BB2E31', '#92AD78','#7295B9', '#D36985'), c(2,1,3,4,6,5), colorblind=F),
+               'Whisper-Rock'=list(c('#1F2921', '#51483F', '#9AD8C6', '#ADCF88'), c(2,4,3,1), colorblind=T),
+               'Porco-Garden'=list(c('#B8484E', '#619D55', '#D6C57A', '#1F4C39', '#C87054', '#9FB67D', '#005D91'), c(3,2,6,7,4,5,1), colorblind=F),
+               'Whisper-Roads'=list(c('#41506D', '#332211', '#64270B', '#7F4F2A', '#C55E56', '#C38F48', '#2D606D'), c(6,3,1,2,5,4,7), colorblind=F),
+               'Chihiro-Banquet'=list(c('#648B45', '#100E0C', '#E6454D', '#EBB754', '#698DA0', '#97D690', '#675680'), c(6,7,1,3,5,4,2), colorblind=F),
+               'Ponyo-Trash'=list(c('#E26374', '#DDB556', '#69A5C9', '#A3C185', '#89D1CA', '#6A8E47', '#915869'), c(2,4,1,3,6,7,5), colorblind=T),
+               'Sky-Pirate'=list(c('#7A4780', '#E38120', '#5297B0', '#8C214C','#036C67', '#B87E01', '#0A2E52', '#E5847D'), c(4,5,7,1,8,6,2,3), colorblind=F),
+               'Kaguya-Kimono'=list(c('#EA995C', '#FBE6AF', '#9CB07F', '#C45A78', '#70548E', '#B6988E', '#9BB9D1'), c(2,5,6,1,4,7,3), colorblind=F),
+               'Ponyo-Goddess'=list(c('#2B43A3', '#FFFEC8', '#F18176', '#8CDBF0', '#60384A'), c(1,2,3,4,5), colorblind=T),
+               'Howl-Heart'=list(c('#C6D3D9', '#92A4A8', '#3D799E', '#F77663', '#FCEE74'), c(5,1,2,3,4), colorblind=T))
+
+colorblind_ghib_palettes <- c("Totoro-Umbrella", "Mononoke-Hug", "Mononoke-Knife","Porco-Cave","Chihiro-Lump",
+                              "Sky-Necklace", "Nausicaa-Bug", "Howl-Star",  "Totoro-Catbus",  "Wind-Timer",
+                              "Earthsea-Silk", "Arrietty-Garden", "Mark-Car", "Cat-Bubble", "Whisper-Rock",
+                              "Ponyo-Trash",  "Ponyo-Goddess",   "Howl-Heart")
+
+ghibli.brewer <- function(palette_name, n, type = c("discrete", "continuous"), direction = c('default', 'reverse', 'random'), override_order=FALSE, return_hex=FALSE) {
+  
+  `%notin%` <- Negate(`%in%`)
+  
+  if (is.null(palette)|is.numeric(palette_name)){
+    stop("Palette is missing or does not exist.")
+  }
+  
+  palette <- Ghibli[[palette_name]]
+
+  if (missing(n)) {
+    n <- length(palette[[1]])
+  }
+  
+  if (is.numeric(n)==F){
+    n <- length(palette[[1]])
+    print('WARNING: n is not properly specified for this use of ghibli.brewer()')
+  }
+  #type <- match.arg(type)
+
+  if (missing(type)){type <- 'Missing'}
+  
+  if (missing(direction)){
+    if (type%in%c('default', 'reverse', 'random')){
+      direction <- type
+      print('WARNING: Your direction argument is not correctly specified, please review the function arguments call')
+      }
+    else{direction <- 'default'}
+  }
+  
+  if (direction %notin% c('default', 'reverse', 'random')){
+    stop("Direction not valid. Please use 'default' for standard palette, 'reverse' for reversed palette or 'random' for random order.")
+  } 
+  
+  
+  temptype <- type
+  
+  if(direction%in%c("discrete", "continuous")){
+    type <- direction
+  }
+  
+  if(temptype%in%c('default', 'reverse', 'random')){
+    direction <- temptype
+  }
+  
+  if (type%notin%c('continuous', 'discrete')) {
+    if(n > length(palette[[1]])){type <- "continuous"}
+    else{type <- "discrete"}
+  }
+    
+  if (type == "discrete" && n > length(palette[[1]])) {
+    stop("Number of requested colors greater than what discrete palette can offer, \n use continuous instead.")
+  }
+  
+  continuous <- if(direction=='default'){grDevices::colorRampPalette(palette[[1]])(n)
+  } else if(direction=='reverse'){
+    grDevices::colorRampPalette(rev(palette[[1]]))(n)} else if(direction=='random'){
+      grDevices::colorRampPalette(sample(palette[[1]], replace=F))(n)}
+  
+  discrete <- if(direction=='default' & override_order==FALSE){
+    palette[[1]][which(palette[[2]] %in% c(1:n)==TRUE)]
+  }else if(direction=='reverse' & override_order==FALSE){
+    rev(palette[[1]][which(palette[[2]] %in% c(1:n)==TRUE)])
+  } else if(direction=='default' & override_order==TRUE){
+    palette[[1]][1:n]
+  } else if(direction=='reverse' & override_order==TRUE){
+    rev(palette[[1]][1:n])
+  } else{
+    sample(palette[[1]], replace=F, n)
+  }
+  
+  out <- switch(type,
+                continuous = continuous,
+                discrete = discrete
+  )
+  if(return_hex==T|direction=='random'){print(out)}
+  structure(out, class = "palette", name = palette_name)
+}
+
+#' @export
+#' @importFrom grDevices rgb
+#' @importFrom graphics rect par image text
+
+
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+  
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+  
+  rect(0, 0.92, n + 1, 1.08, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 2.5, family = "bookman")
+} 
+
+display.ghibli <- function(n, colorblind_only = FALSE, direction = 'default', override_order=FALSE){
+  if(colorblind_only){
+    N = length(colorblind_ghib_palettes)
+    pal_names = colorblind_ghib_palettes
+  }else{
+    N = length(Ghibli)
+    pal_names = names(Ghibli)
+  }
+  
+  orig_pars <- par()
+  
+  plot_palette = function(name,n){
+    par(mar = c(0.1,0.1,1,0.1))
+    nn <- ifelse(missing(n), length(ghibli.brewer(name)), n)
+    plot(0,type='n',bty='n',xaxt='n',yaxt='n',xlab='',ylab='',
+         ylim = c(0,1),xlim=c(0,nn), main = name)
+    for(j in 1:nn){
+      polygon(x = c(j-1,j-1,j,j),
+              y = c(0,1,1,0),
+              border = NA,
+              col = ghibli.brewer(name, nn, direction= direction,
+                                  override_order=override_order)[j]) 
+    }
+  }
+  
+  if(missing(n)){
+      
+      if(colorblind_only){
+        layout(matrix(1:N,6,3))
+        for(i in 1:N) {plot_palette(pal_names[i])}
+        
+      }else{
+        
+        layout(matrix(1:60,10,6))
+        for(i in 1:60) {plot_palette(pal_names[i])}
+      }
+      
+    } else{
+      
+      if(colorblind_only){
+        
+        layout(matrix(1:N,6,3))
+        for(i in 1:N) {plot_palette(pal_names[i], n)}
+        
+      }else{
+        
+        layout(matrix(1:N,10,6))
+        for(i in 1:N) {plot_palette(pal_names[i], n)}
+        
+      }
+      
+    }
+    
+    layout(matrix(1,1,1))
+    par(mar = orig_pars$mar)
+    
+  
+}
+
+scale_color_ghib_c <- function(palette_name, direction='default', ...){
+  
+  `%notin%` <- Negate(`%in%`)
+  
+  if (direction %notin% c('default', 'reverse', 'random')){
+    stop("Direction not valid. Please use default, reverse or random")
+  }
+  
+  scale_color_gradientn(colors=ghibli.brewer(palette_name=palette_name, direction=direction, override_order = F),
+                        ...)
+}
+
+scale_color_ghib_d <- function(palette_name, n=n, direction='default', ...){
+  
+  `%notin%` <- Negate(`%in%`)
+  
+  if (direction %notin% c('default', 'reverse', 'random')){
+    stop("Direction not valid. Please use default, reverse or random")
+  }
+  
+  scale_color_manual(values=ghibli.brewer(palette_name=palette_name, n=n, direction=direction, type = 'discrete', override_order = F))
+}
+
+scale_fill_ghib_c <- function(palette_name, direction='default', ...){
+  
+  `%notin%` <- Negate(`%in%`)
+  
+  if (direction %notin% c('default', 'reverse', 'random')){
+    stop("Direction not valid. Please use default, reverse or random")
+  }
+  
+  scale_fill_gradientn(colors=ghibli.brewer(palette_name=palette_name, direction=direction, override_order = F),
+                        ...)
+}
+
+scale_fill_ghib_d <- function(palette_name, n=n, direction='default', ...){
+  
+  `%notin%` <- Negate(`%in%`)
+  
+  if (direction %notin% c('default', 'reverse', 'random')){
+    stop("Direction not valid. Please use default, reverse or random")
+  }
+  
+  scale_fill_manual(values=ghibli.brewer(palette_name=palette_name, n=n, direction=direction, type = 'discrete', override_order = F))
+}
+
+plot.colorblind.ghib <- function(name='Totoro', n, direction = 'default'){
+  palette <- ghibli.brewer(name, n, 'discrete', direction = direction)
+
+  deu = colorspace::deutan(palette, severity = 1)
+  pro = colorspace::protan(palette, severity = 1)
+  tri = colorspace::tritan(palette, severity = 1)
+  
+  plot_palette2 = function(x,n, title){
+    par(mar = c(0.1,0.1,1,0.1))
+    nn <- ifelse(missing(n), length(palette), n)
+    plot(0,type='n',bty='n',xaxt='n',yaxt='n',xlab='',ylab='',
+         ylim = c(0,1),xlim=c(0,nn), main = title)
+    for(j in 1:nn){
+      polygon(x = c(j-1,j-1,j,j),
+              y = c(0,1,1,0),
+              border = NA,
+              col = x[j]) 
+    }
+  }
+  
+  par(mar = c(1,1,1,1))
+  layout(matrix(1:4,4,1))
+  plot_palette2(palette, n, title=name)
+  plot_palette2(deu, n, title='Deuteranopia')
+  plot_palette2(pro, n, title='Protanopia')
+  plot_palette2(tri, n, title='Tritanopia')
+  
+  par(mar = c(5, 4, 4, 2) + 0.1)
+  par(mfrow = c(1, 1))
+  
+}
+
+plot.colorblind.palette <- function(palette){
+  
+  deu = colorspace::deutan(palette, severity = 1)
+  pro = colorspace::protan(palette, severity = 1)
+  tri = colorspace::tritan(palette, severity = 1)
+  
+  plot_palette2 = function(x, title){
+    par(mar = c(0.1,0.1,1,0.1))
+    nn <- length(palette)
+    plot(0,type='n',bty='n',xaxt='n',yaxt='n',xlab='',ylab='',
+         ylim = c(0,1),xlim=c(0,nn), main = title)
+    for(j in 1:nn){
+      polygon(x = c(j-1,j-1,j,j),
+              y = c(0,1,1,0),
+              border = NA,
+              col = x[j]) 
+    }
+  }
+  
+  par(mar = c(1,1,1,1))
+  layout(matrix(1:4,4,1))
+  plot_palette2(palette, title='Original')
+  plot_palette2(deu, title='Deuteranopia')
+  plot_palette2(pro, title='Protanopia')
+  plot_palette2(tri, title='Tritanopia')
+  
+  par(mar = c(5, 4, 4, 2) + 0.1)
+  par(mfrow = c(1, 1))
+  
+}
+
+game.ghib <- function(lvl=1, rules=T){
+  films <- sapply(strsplit(names(Ghibli), "-"), `[`, 1)
+  if(rules==T){
+    cat('Welcome to the Ghibli Brewer Game!\nYou have to guess the film of a random palette right.\nGood luck!\n\nYou chose level ',lvl,':\n')
+    if(lvl==1){
+      cat('The palette have 4 colours and the direction is default.\n\n')
+    } else if(lvl==2){
+      cat('The palette have 3 colours and the direction is default.\n\n')
+    } else if(lvl==3){
+      cat('The palette have 2 colours and the direction is default.\n\n')
+    } else if(lvl==4){
+      cat('The palette have 1 colours and the direction is default.\n\n')
+    } else if(lvl==5){
+      cat('The palette have 4 colours and the direction is random.\n\n')
+    } else if(lvl==6){
+      cat('The palette have 3 colours and the direction is random.\n\n')
+    } else if(lvl==7){
+      cat('The palette have 2 colours and the direction is random.\n\n')
+    } else if(lvl==8){
+      cat('The palette have 1 colours and the direction is random.\n\n')
+    } else{
+      cat('Your level is not a number between 1 or 8, so you have been set to lvl1.\n\n')
+    } 
+    
+    cat(paste('These are the included films: ', paste(unique(films), collapse = ", ")))
+    cat("\n\n")
+  }else{}
+  
+  lotto <- sample(1:49, 1)
+  name <- names(Ghibli)[lotto]
+
+  
+  plot_palette3 = function(x,n){
+    par(mar = c(0.1,0.1,1,0.1))
+    nn <- length(palette)
+    plot(0,type='n',bty='n',xaxt='n',yaxt='n',xlab='',ylab='',
+         ylim = c(0,1),xlim=c(0,nn))
+    for(j in 1:nn){
+      polygon(x = c(j-1,j-1,j,j),
+              y = c(0,1,1,0),
+              border = NA,
+              col = x[j]) 
+    }
+  }
+  
+  if(lvl==1){
+    n <- 4
+    direct <- 'default'
+  } else if(lvl==2){
+    n <- 3
+    direct <- 'default'
+  } else if(lvl==3){
+    n <- 2
+    direct <- 'default'
+  } else if(lvl==4){
+    n <- 1
+    direct <- 'default'
+  } else if(lvl==5){
+    n <- 4
+    direct <- 'random'
+  } else if(lvl==6){
+    n <- 3
+    direct <- 'random'
+  } else if(lvl==7){
+    n <- 2
+    direct <- 'random'
+  } else if(lvl==8){
+    n <- 1
+    direct <- 'random'
+  } else{
+    n <- 4
+    direct <- 'default'
+  }
+  
+  palette <- ghibli.brewer(name, n=n, direction=direct)
+  
+  plot_palette3(palette, n)
+
+  guess <- readline(prompt = "What is the film of this palette? ")
+  if (tolower(guess) == tolower(films[lotto])) {
+    cat(paste("Congratulations! You guessed it right! It is: ", name,"\n"))
+    if(lvl==8){
+      cat("You completed the game, you are a true GhibliBrewer expert now!\n")
+    }
+    else{
+      KG <- readline(prompt = "Do you want to keep going? (Y/N): ")
+      if(KG=='Y'){
+        return(game.ghib(lvl = lvl+1))
+      }
+      else{
+        return(cat())
+      }
+      # Return TRUE if the guess is correct
+    }
+  } else {
+    return(cat(paste("Sorry, that's not correct. The correct palette is:", name, ", try again!\n")))  # Return FALSE if the guess is incorrect
+  }
+  
+}
